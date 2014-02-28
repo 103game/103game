2b544ed1cb9c6c94449104c68edfe7c5da40dc69
Aldrog <penkrat8@gmail.com>
2014-02-28 23:47:47 +0400

    Added ZeroMQ example

--- server/server_app/main.cpp
+++ server/server_app/main.cpp
@@ -1,11 +1,32 @@
-#include <iostream>
-#include <zmq.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "zmq.h"
 
-
-using namespace std;
-
-int main(void)
+int main (int argc, char const *argv[])
 {
-	
-	return 0;
+    void* context = zmq_ctx_new();
+    void* respond = zmq_socket(context, ZMQ_REP);
+
+    zmq_bind(respond, "tcp://*:4040");
+    printf("Starting…\n");
+
+    for(;;)
+    {
+        zmq_msg_t request;
+        zmq_msg_init(&request);
+        zmq_msg_recv(&request, respond, 0);
+        printf("Received: hello\n");
+        zmq_msg_close(&request);
+        sleep(1); // sleep one second
+        zmq_msg_t reply;
+        zmq_msg_init_size(&reply, strlen("world"));
+        memcpy(zmq_msg_data(&reply), "world", 5);
+        zmq_msg_send(&reply, respond, 0);
+        zmq_msg_close(&reply);
+    }
+    zmq_close(respond);
+    zmq_ctx_destroy(context);
+
+    return 0;
 }

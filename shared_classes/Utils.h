4fc20f59c88211dd5c1358ad0185f520157ce8a2
VMozin <artec457@mail.ru>
2014-05-15 22:14:26 +0400

    adding function_time

--- shared_classes/Utils.h
+++ shared_classes/Utils.h
@@ -137,6 +137,35 @@ public:
 		return !(DotOffset >= ((int)Length-1));
 	}
 
+	/*
+	Convert tm structure to string
+	Interprets the contents of the tm structure pointed by timeptr as a calendar time 
+	and converts it to a C-string containing a human-readable version of the corresponding 
+	date and time.
+	*/
+	char* asctime(const struct tm *timeptr)
+	{
+		static const char wday_name[][4] = {
+		"Sun", "Mon", "Tue", 
+		"Wed", "Thu", "Fri", 
+		"Sat"
+	};
+	static const char mon_name[][4] = {
+		"Jan", "Feb", "Mar", 
+		"Apr", "May", "Jun",
+		"Jul", "Aug", "Sep", 
+		"Oct", "Nov", "Dec"
+	};
+		static char result[26];
+		sprintf(result, "%.3s %.3s%3d %.2d:%.2d:%.2d %d\n",
+		wday_name[timeptr->tm_wday],
+		mon_name[timeptr->tm_mon],
+		timeptr->tm_mday, timeptr->tm_hour,
+		timeptr->tm_min, timeptr->tm_sec,
+		1900 + timeptr->tm_year);
+		return result;
+	}
+
 };
 
 
